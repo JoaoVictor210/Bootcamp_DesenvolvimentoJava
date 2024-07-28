4295131a63a1daebf2db3ac9f565cca57484af4e
joao <joaovictor040220@gmail.com>
2024-07-28 06:49:49 -0300

    java

---
+++ boot_camp/src/javabasico/Metodos/aula01/MinhaClass.java
@@ -0,0 +1,10 @@
+package javabasico.Metodos.aula01;
+
+public class MinhaClass {
+    public static void main(String[] args) {
+        OlaMundo();
+    }
+    public static void OlaMundo(){
+        System.out.println("Olá, Mundo!");
+    }
+}
---
+++ boot_camp/src/javabasico/Metodos/aula02/MinhaClass.java
@@ -0,0 +1,7 @@
+package javabasico.Metodos.aula02;
+
+public class MinhaClass {
+    public static void main(String[] args) {
+
+    }
+}
